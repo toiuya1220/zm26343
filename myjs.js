8b026a59667b5c4e7b0853398089fea44b6d3ee2
toiuya1220 <zm26343@josai.ac.jp>
2026-06-22 12:17:14 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-  </head>
-<body>
-  alert('Hello, touya!');
+ alert('Hello, touya!');
  alert(1+1);
  alert(Math.sqrt(1+1));
-</body>
-  </html>
+
